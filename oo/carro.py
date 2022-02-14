b9ae3b6a52869e7562a7da77113fe9b9e4209044
Sastukye <ghcweb@hotmail.com>
2022-02-14 11:30:52 -0300

    Finalização do projeto carro, onde viza o aprendizado sobre a composição do projeto e que é um tema importante de ficar gravado na cabeça do  aluno.

--- oo/carro.py
+++ oo/carro.py
@@ -67,7 +67,7 @@ O       L
     >>> direcao.valor
     'Norte'
     >>> carro= Carro(direcao, motor)
-    >>> carro.calcular_velocidade
+    >>> carro.calcular_velocidade()
     0
     >>> carro.acelerar()
     >>> carro.calcular_velocidade()
@@ -79,7 +79,7 @@ O       L
     >>> carro.calcular_velocidade()
     0
     >>> carro.calcular_direcao()
-    'Norte
+    'Norte'
     >>> carro.girar_a_direita()
     >>> carro.calcular_direcao()
     'Leste'
@@ -90,6 +90,30 @@ O       L
     >>> carro.calcular_direcao()
     'Oeste'
 """
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
 
 
 class Motor:
